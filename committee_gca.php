8ae711e3809c99436818193dd1390ae87899106f
Ashok <ashok.x.kumar@marks-and-spencer.com>
2021-08-19 08:56:31 +0100

    Initial Commit

---
+++ committee_gca.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<?php include "css.php";?>
+</head>
+<body>
+<?php include "header.php";?>
+
+<div class="slider-portion wisinsli">
+<div class="slider1">
+   <div class="slide animation-edu-3">
+			<h1>Global Center of Arts</h1>
+     </div>
+  </div>
+  </div>
+
+<div class="clearfix"></div>
+<div class="wisewoods-content">
+<div class="container">
+<div class="heading ani">
+<h1>Executive <span>Committee</span></h1>
+</div>
+<!--study bg-->
+<div class="clearfix"></div>
+
+<div class="study-bg ani">
+<div class="name-profile"> <h2>Mrs. Uma Devi Ashok Kumar<span>Founder,
+Global Tamil School, UK
+Global Academy of Arts</span></h2>  </div>
+<div class="col-md-12 no-pad-img border-cont ani">
+<div class="border-cont-br border-cont-rightco">
+<img src="img/people/Photo_UmaAshok.png" alt="Founder" class="img-responsive"/>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+<div class="clearfix"></div>
+</div>
+</div>
+<div class="clearfix"></div>
+</div>
+<div class="study-bg ani">
+<div class="name-profile"> <h2>Mrs. Udhaya Bhoopathy<span>Assistant Director,
+Global Academy of Arts, India</span></h2> </div>
+<div class="col-md-12 no-pad-img border-cont ani">
+<div class="border-cont-br border-cont-rightco">
+<img src="img/people/Photo_UdhayaBoopathy.png" alt="chairman" class="img-responsive"/>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+<div class="clearfix"></div>
+</div>
+</div>
+<div class="clearfix"></div>
+</div>
+<div class="study-bg ani">
+<div class="name-profile"> <h2>Mrs. Jayaveerapandian<span>Assistant Director,
+Global Academy of Arts, Krishnagiri District
+</span></h2> </div>
+<div class="col-md-12 no-pad-img border-cont ani">
+<div class="border-cont-br border-cont-rightco">
+<img src="img/people/Photo_Jay.png" alt="chairman" class="img-responsive"/>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+<div class="clearfix"></div>
+</div>
+</div>
+<div class="clearfix"></div>
+</div>
+</div>
+</div>
+<?php include "footer.php";?>
+
+<?php include "js.php";?>
+
+</body>
+</html>
